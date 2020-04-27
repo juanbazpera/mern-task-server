bcd10079affd5998aadafdef0d33a2ae7e301300
Juan Baz <juan.baz.pera@gmail.com>
2020-04-27 02:29:16 -0300

    add heroku domain & port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // APP port
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // Routes
 app.use('/api/users', require('./routes/users'));
@@ -22,6 +22,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-app.listen(PORT, () => {
-  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
